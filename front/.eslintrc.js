9ffe984820922b7a1a55985d45f3318626a8c0aa
huanghaijun <huanghaijun@itouchtv.cn>
2021-06-08 13:53:01 +0800

    PRETTIER: plugin:prettier/recommended

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -3,15 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'plugin:prettier/recommended'
   ],
   rules: {
+    'comma-dangle': ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'none' }]
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
