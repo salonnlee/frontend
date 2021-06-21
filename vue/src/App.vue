c2e5fd322c4a64178a238132241c16bf5e505bae
huanghaijun <huanghaijun@itouchtv.cn>
2021-06-22 00:44:17 +0800

    Vue 使用：v-for、v-if、watch

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <watch></watch>
+    <for-if></for-if>
+  </div>
 </template>
 
 <script>
+import Watch from "./components/watch.vue";
+import ForIf from "./components/for-if.vue";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    Watch,
+    ForIf
+  }
 };
 </script>
+
+<style>
+html,
+body {
+  font-family: "Fira Code";
+}
+</style>
---
+++ vue/src/components/for-if.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="for-if">
+    <ul>
+      <!-- eslint-disable vue/no-use-v-if-with-v-for -->
+      <li
+        v-for="(item, index) in (log('v-for'), list)"
+        :key="item.id"
+        v-if="(log('v-if'), index > 1)"
+      >
+        {{ index }}. id:{{ (log('li'), item.id) }} title:{{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ForIf",
+  data: function() {
+    return {
+      list: [
+        { id: 1, title: "I am FIRST" },
+        { id: 2, title: "I am SECONDE" },
+        { id: 3, title: "I am THIRD" }
+      ]
+    };
+  },
+  methods: {
+    log(...args) {
+      console.log(...args);
+    }
+  }
+};
+</script>
