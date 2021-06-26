7f945c7dc26afa6b200968f2adb2507b57fc9280
huanghaijun <huanghaijun@itouchtv.cn>
2021-06-26 13:48:04 +0800

    Template and vdom-diff

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <!-- <watch></watch> -->
     <!-- <for-if></for-if> -->
-    <observer></observer>
+    <!-- <observer></observer> -->
+    <!-- <vdom-diff></vdom-diff> -->
+    <vue-template></vue-template>
   </div>
 </template>
 
@@ -10,6 +12,8 @@
 import Watch from "./components/watch.vue";
 import ForIf from "./components/for-if.vue";
 import Observer from "./components/observer.vue";
+import VdomDiff from "./components/vdom-diff.vue";
+import VueTemplate from './components/template.vue'
 
 export default {
   name: "App",
@@ -17,8 +21,10 @@ export default {
     /* eslint-disable vue/no-unused-components */
     Watch,
     ForIf,
+    Observer,
+    VdomDiff,
     /* eslint-enable vue/no-unused-components */
-    Observer
+    VueTemplate
   }
 };
 </script>
---
+++ vue/src/components/template.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="vue-template"></div>
+</template>
+
+<script>
+export default {
+  name: "VueTemplate",
+  mounted() {
+    const compiler = require("vue-template-compiler");
+
+    const template1 = "<p>{{message}}</p>";
+    // with(this) { return _c("p", [_v(_s(message))]); }
+    // _c = createElement, _v = createTextVNode, _s = toString, return vnode
+
+    const template2 =
+      '<div id="div1" class="container"><img :src="imgUrl" @click="handleImgClick" /></div>';
+    // with(this) { return _c("div",
+    //                       { staticClass: "container", attrs: { id: "div1" } },
+    //                       [_c("img", { attrs: { src: imgUrl }, on: { click: handleImgClick } })]); }
+
+    const template3 = '<input type="text" v-model="title" />';
+    // with(this) { return _c("input",
+    //                        { directives: [{ name: "model", rawName: "v-model",
+    //                          value: title, expression: "title" }],
+    //                          attrs: { type: "text" },
+    //                          domProps: { value: title },
+    //                          on: { input: function ($event) { if ($event.target.composing) return; title = $event.target.value; } }
+    //                        }); }
+
+    const { render: render1 } = compiler.compile(template1);
+    const { render: render2 } = compiler.compile(template2);
+    const { render: render3 } = compiler.compile(template3);
+    console.warn(render1);
+    console.warn(render2);
+    console.warn(render3);
+    // render = function () { with(this) { ... }; }
+  }
+};
+</script>
