7a528b48c505a3a15fb899ad3a0ca7740c22749f
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-08-17 09:52:07 +0800

    Quill editor

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,12 @@
+import "./app.css";
+import "./quill/index";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="editor" contentEditable="true"></div>
+    </div>
+  );
 }
 
 export default App;
