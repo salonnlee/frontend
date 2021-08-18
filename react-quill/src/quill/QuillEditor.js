7fa70a5f461e35cef4366cd69bc4961c3a64b96c
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-08-18 17:11:32 +0800

    MutationObserver

--- react-quill/src/quill/QuillEditor.js
+++ react-quill/src/quill/QuillEditor.js
@@ -2,7 +2,22 @@ import React from "react";
 
 import "./quilleditor.css";
 
+const OBSERVER_CONFIG = {
+  attributes: true,
+  characterData: true,
+  characterDataOldValue: true,
+  childList: true,
+  subtree: true
+};
+
 class QuillEditor extends React.Component {
+  componentDidMount() {
+    this.domNode = document.getElementsByClassName("editor")[0];
+    this.observer = new MutationObserver((mutations) => {
+      console.warn("observed domNode mutated!", [mutations[0]]);
+    });
+    this.observer.observe(this.domNode, OBSERVER_CONFIG);
+  }
   render() {
     return <div className="editor" contentEditable="true"></div>;
   }
