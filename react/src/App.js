31cae4496921e12dc9e6c2f66fb4be78a3086557
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-08-20 10:50:23 +0800

    QuillEditor placehold

--- react/src/App.js
+++ react/src/App.js
@@ -1,36 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
-
-// import "./quill/index";
-// import "./rxjs/index";
+import QuillEditor from "./QuillEditor";
 
 import "./app.css";
 
-const mapStateToProps = (state, ownProps) => {
-  console.warn("mapStateToProps args", state, ownProps);
-  return { ...state };
-};
-const mapDispatchToProps = (dispatch, ownProps) => {
-  console.warn("mapDispatchToProps args", dispatch, ownProps);
-  return {
-    fetch: () => {
-      dispatch({ type: "FETCH_TEST" });
-    }
-  };
-};
-
-const App = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(function App(props) {
-  console.log("App", props);
-  const { fetch } = props;
+const App = function App(props) {
   return (
     <div className="App">
-      <button onClick={() => fetch('hahhahha')}>Click me!</button>
-      {/* <div className="editor" contentEditable="true"></div> */}
+      <QuillEditor />
     </div>
   );
-});
+};
 
 export default App;
---
+++ react/src/QuillEditor/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class QuillEditor extends React.Component {
+  render() {
+    return <div className="editor" contentEditable="true"></div>;
+  }
+}
+
+export default QuillEditor;
