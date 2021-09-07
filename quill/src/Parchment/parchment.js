306d7b946326a3288cf4c23d401d1752bed589a5
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-09-07 12:16:53 +0800

    format

--- quill/src/Parchment/parchment.js
+++ quill/src/Parchment/parchment.js
@@ -2,6 +2,7 @@ import Attributor from "./attributor/attributor";
 import ClassAttributor from "./attributor/class";
 import StyleAttributor from "./attributor/style";
 import AttributorStore from "./attributor/store";
+
 import Registry from "./registry";
 import Scope from "./scope";
 
