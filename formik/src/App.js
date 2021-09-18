9e54c25dfbdd432863611e5f4d6baa334ccc79d0
huanghaijun <cloud_huanghaiyun@youzan.com>
2021-09-18 18:43:44 +0800

    Formik SignupForm

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import SignupForm from "./containers/SignupForm";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <SignupForm />
+    </div>
+  );
 }
 
 export default App;
